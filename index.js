9f15cdf47023f96d20c96bef9ecdfc7e3077c6f4
Matheus Rodrigues <matheusrf96@gmail.com>
2019-01-26 14:38:15 -0300

    Add view engine mustache

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const path = require('path');
+const hoganMiddleware = require('hogan-middleware');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+app.engine('mustache', hoganMiddleware.__express);
+
 app.get('/', (req, res, next) => {
     res.send('Hello! :)');
+});
+
+app.get('/json', (req, res, next) => {
+    const data = {
+        key: 'Hello! :)',
+    }
+
+    res.json(data);
+});
+
+app.get('/home', (req, res, next) => {
+    res.render('home', null);
 });
 
 app.listen(3000);
